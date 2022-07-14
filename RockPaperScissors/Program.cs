1958c523ba431334ef0d19ab13138a4e0f557983
karolinajoanna <karolina.j.osuch@gmail.com>
2022-07-14 13:44:11 +0200

    Newborn project

---
+++ RockPaperScissors/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace RockPaperScissors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class Picatchu
+    {
+         public void Say()
+        {
+            Console.WriteLine("Pica pica picaaaatchu!");
+        }
+    }
+}
